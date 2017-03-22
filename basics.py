930207ea4df3adc3afb83a48fd46d21a92eb5eb2
zhaobe <zhaoben777@gmail.com>
2017-03-21 22:07:52 -0700

    Adding on to the basics of splinter.

--- basics.py
+++ basics.py
@@ -2,7 +2,19 @@ from splinter import Browser
 
 browser = Browser()
 
+# visit site
+browser.visit('http://google.com')
+browser.visit('http://selenium-python.readthedocs.io/')
 browser.visit('https://splinter.readthedocs.io/')
+
+# browser navigation
+browser.back()
+
+# saving vars
+titleOfBrowser = browser.title
+urlOfBrowser = browser.url
+
+browser.forward()
 
 # testing wait_time
 browser.is_text_present('splinter', wait_time=10)
@@ -11,3 +23,7 @@ browser.is_text_present('splinter', wait_time=10)
 #browser.execute_script("alert('Hello')")
 
 browser.quit()
+
+# should output the second site info
+print titleOfBrowser
+print urlOfBrowser
